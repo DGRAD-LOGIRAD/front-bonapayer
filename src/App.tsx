4aead2c8dac003400a1dd9ea987bd782f5b6441b
mechack tambwe <mesutarso@users.noreply.github.com>
2025-10-16 18:00:59 +0100

    fix: some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Preloader } from '@/components/ui/preloader';
-import { ErrorTest } from '@/components/ui/error-test';
 
 function App() {
   return (
     <>
       <Preloader />
       <Outlet />
-      <ErrorTest />
     </>
   );
 }
